c90fb41388a29e9b8764c4fd8d1d107923b61118
Sandali Herath <87419212+IT21114144@users.noreply.github.com>
2021-07-14 10:28:18 +0000

    I have completed first Question

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+  float mrk1 , mrk2 , tot , avg ;
+
+  printf("\nEnter mark 1 : ") ;
+  scanf("%f" , &mrk1) ;
+
+  printf("Enter mark 2 : ") ;
+  scanf("%f" , &mrk2) ;
+
+  tot = mrk1 + mrk2 ;
+  avg = tot / 2 ;
+
+  printf("\nAverage is %.2f " , avg ) ;
   
   return 0;
 }
